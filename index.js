e1a5db64d1da2e3ac0176e2b98ded67e7a370cdc
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-12-22 16:24:29 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const port = 53134;
+
+http.createServer((req, res) => {
+	let responseCode = 404;
+	let content = '404 Error';
+
+	if (req.url === '/') {
+		responseCode = 200;
+		content = fs.readFileSync('./index.html');
+	}
+
+	res.writeHead(responseCode, {
+		'content-type': 'text/html;charset=utf-8',
+	});
+
+	res.write(content);
+	res.end();
+})
+	.listen(port);
